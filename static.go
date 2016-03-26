72353857d0a52844a7fad033a5e194c21a2d4923
Rex Lee(李俊) <duguying2008@gmail.com>
2016-03-26 22:50:21 +0800

    static serve for 'not modified' status 304

--- static.go
+++ static.go
@@ -2,7 +2,6 @@ package ela
 
 import (
 	"github.com/elago/ela/debug"
-	"github.com/gogather/com"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -13,47 +12,46 @@ const (
 )
 
 func StaticServ(uri string, writer http.ResponseWriter, request *http.Request) {
-	path := filepath.Join(staticDirectory, uri)
-	stat, err := os.Stat(path)
+	path := filepath.Join(uri)
+	stat, err := os.Stat(filepath.Join(staticDirectory, path))
 	if err != nil {
 		// 404
-		debug.RequestLog(400, "static", request.Method, path)
+		debug.RequestLog(404, "static", request.Method, path)
 		http.Error(writer, "404, File Not Exist", 404)
 		return
 	}
 
 	if !stat.IsDir() {
 		// read file
-		data, err := com.ReadFileByte(path)
-		if err == nil {
-			debug.RequestLog(200, "static", request.Method, path)
-			writer.Write([]byte(data))
-		} else {
-			// 404
-			debug.RequestLog(400, "static", request.Method, path)
-			http.Error(writer, "404, File Not Exist", 404)
-			return
-		}
+		servPath(path, writer, request)
 	} else {
 		path := filepath.Join(path, "index.html")
-		_, err := os.Stat(path)
+		servPath(path, writer, request)
+	}
+
+}
+
+func servPath(path string, writer http.ResponseWriter, request *http.Request) {
+	FileSystem := newStaticFileSystem(staticDirectory)
+	f, err := FileSystem.Open(path)
+
+	if err != nil {
+		// 404
+		debug.RequestLog(404, "static", request.Method, path)
+		http.Error(writer, "404, File Not Exist", 404)
+		return
+	} else {
+		fi, err := f.Stat()
 		if err != nil {
+			// File exists but fail to open.
 			// 404
-			debug.RequestLog(400, "static", request.Method, path)
+			debug.RequestLog(404, "static", request.Method, path)
 			http.Error(writer, "404, File Not Exist", 404)
 			return
 		}
 
-		data, err := com.ReadFileByte(path)
-		if err == nil {
-			writer.Write([]byte(data))
-		} else {
-			// 404
-			debug.RequestLog(400, "static", request.Method, path)
-			http.Error(writer, "404, File Not Exist", 404)
-		}
+		http.ServeContent(writer, request, path, fi.ModTime(), f)
 	}
-
 }
 
 func StaticExist(uri string) bool {
@@ -64,4 +62,26 @@ func StaticExist(uri string) bool {
 	} else {
 		return true
 	}
+}
+
+// staticFileSystem implements http.FileSystem interface.
+type staticFileSystem struct {
+	dir *http.Dir
+}
+
+func newStaticFileSystem(directory string) staticFileSystem {
+	Root, err := os.Getwd()
+	if err != nil {
+		panic("error getting work directory: " + err.Error())
+	}
+
+	if !filepath.IsAbs(directory) {
+		directory = filepath.Join(Root, directory)
+	}
+	dir := http.Dir(directory)
+	return staticFileSystem{&dir}
+}
+
+func (fs staticFileSystem) Open(name string) (http.File, error) {
+	return fs.dir.Open(name)
 }
