6ef02549add991933328063b6eb0f4083f7af3eb
Rex Lee(李俊) <duguying2008@gmail.com>
2016-05-18 21:29:00 +0800

    http_test for dependency injection middleware

---
+++ http_test.go
@@ -0,0 +1,29 @@
+package ela
+
+import (
+	"github.com/gogather/com/log"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+type MidStruct1 struct {
+	name string
+	age  int
+}
+
+func action(m MidStruct1, t *testing.T) {
+	log.Println(m.name)
+	if m.name != "lee" {
+		t.Error("maybe injection failed")
+	}
+}
+
+func TestInjection(t *testing.T) {
+	log.Debug = true
+	Convey("Injection sections", t, func() {
+		Use(MidStruct1{name: "lee", age: 24})
+		Use(&testing.T{})
+		injectFuc(action)
+	})
+
+}
